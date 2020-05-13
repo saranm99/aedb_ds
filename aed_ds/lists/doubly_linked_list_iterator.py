4cb72d8005d1e1abd8b1df4aa32fdbb393719e3c
saranm99 <58508024+saranm99@users.noreply.github.com>
2020-05-13 20:47:14 +0100

    G02 - Pull request

--- aed_ds/lists/doubly_linked_list_iterator.py
+++ aed_ds/lists/doubly_linked_list_iterator.py
@@ -3,19 +3,25 @@ from .singly_linked_list_iterator import SinglyLinkedListIterator
 from ..exceptions import NoSuchElementException  
 
 class DoublyLinkedListIterator(SinglyLinkedListIterator):
-    def __init__(self, position):
-        pass
+    def __init__(self, position, last):
+        SinglyLinkedListIterator.__init__(self, position)
+        self.last = last
 
     # Returns true iff the iteration has more elements in the reverse direction.
     # In other words, returns true if previous would return an element rather than throwing an exception.
     def has_previous(self): 
-        pass
+        return (self.position != None)
     
     # Returns the previous element in the iteration.
     # Throws NoSuchElementException
     def previous(self): 
-        pass
+        if self.has_previous():
+            element = self.position.get_element()
+            self.position = self.position.get_previous()
+            return element
+        else:
+            raise NoSuchElementException()   
 
     # Restarts the iteration in the reverse direction. After fullForward, if the iteration is not empty, previous will return the last element in the iteration.
     def full_forward(self): 
-        pass
+        self.position = self.last
